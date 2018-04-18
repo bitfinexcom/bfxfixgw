97d5a0cb67ccc855c64b3a8f4f4e972361854b6e
Matt Lane <mlane@connamara.com>
2018-04-18 11:58:51 -0500

    another crack at atomic nonce

--- service/peer/nonce.go
+++ service/peer/nonce.go
@@ -7,44 +7,33 @@ import (
 	"time"
 )
 
+var ts, nonce uint64
+var m sync.Mutex
+
 type MultikeyNonceGenerator struct {
-	ts, nonce uint64
-	m         sync.Mutex
 }
 
 // API key must be exlusively used in this process space.
 // Atomic counter per-time.Now() update supporting up to 999 operations per tick
 func (u *MultikeyNonceGenerator) GetNonce() string {
-	u.m.Lock()
-	var s string
+	m.Lock()
 	n := uint64(time.Now().Unix()) * 1000
-	if u.ts == n {
-		u.nonce++
+	if ts == n {
+		nonce++
 	} else {
-		u.ts = n
-		u.nonce = 1
+		ts = n
+		nonce = 1
 	}
-	s = strconv.FormatUint(u.ts+u.nonce, 10)
-	u.m.Unlock()
+	s := strconv.FormatUint(ts+nonce, 10)
+	m.Unlock()
 	return s
 }
 
 func NewMultikeyNonceGenerator() utils.NonceGenerator {
-	return &MultikeyNonceGenerator{
-		ts:    uint64(time.Now().Unix()) * 1000,
-		nonce: 1,
-	}
-}
-
-// v1 support
-var ts uint64
-var nonce uint64
-
-func reset() {
-	ts = uint64(time.Now().Unix()) * 1000
-	nonce = 1
+	return &MultikeyNonceGenerator{}
 }
 
 func init() {
-	reset()
+	ts = uint64(time.Now().Unix()) * 1000
+	nonce = 1
 }

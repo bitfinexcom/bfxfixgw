a2b82d7b8902d68bf566ec3ec4f73b860696610a
Michael L. Wilner <mike@wilner.me>
2019-06-03 09:33:44 -0500

    Dep ensure for the FIX 5.0 packages

---
+++ vendor/github.com/quickfixgo/fixt11/doc.go
@@ -0,0 +1,6 @@
+/*
+Package fixt11 is generated from the generate-fix tool using standard FIX specs from the github.com/quickfixgo/quickfix package.
+
+User manual and additional information available at http://quickfixgo.org
+*/
+package fixt11
